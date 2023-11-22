15cad3275b0a88b18d88073d0f3c5dbf0f169999
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-22 07:53:14 +0100

    Add project files.

---
+++ Exercise 6.2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string commandLineArgumentString = args[0];
+            int commandLineArgumentInt = int.Parse(args[1]);
+
+
+            Console.WriteLine($"String: {commandLineArgumentString}");
+            Console.WriteLine($"Integer: {commandLineArgumentInt}");
+        }
+    }
+}
